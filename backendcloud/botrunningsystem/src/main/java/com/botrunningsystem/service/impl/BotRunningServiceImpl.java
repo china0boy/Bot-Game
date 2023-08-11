197fddb14bc55a01a04d7aa83fbf6332df4bb32c
china0boy <2723408135@qq.com>
2023-08-11 17:07:52 +0800

    实现执行bot代码的微服务

--- backendcloud/botrunningsystem/src/main/java/com/botrunningsystem/service/impl/BotRunningServiceImpl.java
+++ backendcloud/botrunningsystem/src/main/java/com/botrunningsystem/service/impl/BotRunningServiceImpl.java
@@ -1,12 +1,16 @@
 package com.botrunningsystem.service.impl;
 
+import com.botrunningsystem.service.impl.utils.BotPool;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BotRunningService implements com.botrunningsystem.service.BotRunningService {
+    public static final BotPool botPool = new BotPool();
+
     @Override
     public String addBot(Integer userId, String boyCode, String input) {
         System.out.println("userId: " + userId+ " boyCode: " + boyCode + " input: " + input);
+        botPool.addBot(userId, boyCode, input);
         return "添加bot";
     }
 }
